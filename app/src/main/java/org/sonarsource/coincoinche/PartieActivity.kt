46b7a81d1ad0e9c7fac6207406c34dc8b166d73d
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-06-06 15:21:07 +0200

    First version of manche activity

--- app/src/main/java/org/sonarsource/coincoinche/PartieActivity.kt
+++ app/src/main/java/org/sonarsource/coincoinche/PartieActivity.kt
@@ -1,9 +1,12 @@
 package org.sonarsource.coincoinche
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class PartieActivity : AppCompatActivity() {
 
@@ -13,6 +16,12 @@ class PartieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_partie)
+
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, MancheActivity::class.java).apply {}
+            startActivity(intent)
+        }
 
         listView = findViewById(R.id.manches_list)
         val listItems = arrayOfNulls<String>(24)
