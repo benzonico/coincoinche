f74f4d4ff0b9e0d1d0ec079035319d0688b641d3
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-06-06 13:49:41 +0200

    Add partie activity

--- app/src/main/java/org/sonarsource/coincoinche/MainActivity.kt
+++ app/src/main/java/org/sonarsource/coincoinche/MainActivity.kt
@@ -1,7 +1,7 @@
 package org.sonarsource.coincoinche
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
@@ -16,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val intent = Intent(this, Partie::class.java).apply {}
+            startActivity(intent)
         }
     }
 
---
+++ app/src/main/java/org/sonarsource/coincoinche/Partie.kt
@@ -0,0 +1,12 @@
+package org.sonarsource.coincoinche
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Partie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_partie)
+    }
+}
