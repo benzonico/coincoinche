a76f587c405938f2225616445f42425d16012858
Nicolas PERU <nicolas.peru@sonarsource.com>
2019-06-06 15:58:36 +0200

    Use toggle button for preneur

--- app/src/main/java/org/sonarsource/coincoinche/MancheActivity.kt
+++ app/src/main/java/org/sonarsource/coincoinche/MancheActivity.kt
@@ -2,11 +2,20 @@ package org.sonarsource.coincoinche
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_manche.*
 
 class MancheActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manche)
+        euxButton.setOnClickListener { view ->
+            euxButton.isChecked = true
+            nousButton.isChecked = false
+        }
+        nousButton.setOnClickListener { view ->
+            euxButton.isChecked = false
+            nousButton.isChecked = true
+        }
     }
 }
